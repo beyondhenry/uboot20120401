2f5c84ec8baf02b556ac30b622e3ec20158ad3e2
beyondhenry <wyhhnlb@163.com>
2015-11-19 11:21:11 +0800

    2.3.3移植最新u-boot之修改代码支持Nor Flash

--- arch/arm/lib/board.c
+++ arch/arm/lib/board.c
@@ -262,6 +262,8 @@ unsigned int board_init_f(ulong bootflag)
 	init_fnc_t **init_fnc_ptr;
 	gd_t *id;
 	ulong addr, addr_sp;
+	extern ulong base_sp;
+	
 #ifdef CONFIG_PRAM
 	ulong reg;
 #endif
@@ -436,6 +438,8 @@ unsigned int board_init_f(ulong bootflag)
 	debug("relocation Offset is: %08lx\n", gd->reloc_off);
 	memcpy(id, (void *)gd, sizeof(gd_t));
 
+	base_sp = addr_sp;
+
 	//relocate_code(addr_sp, id, addr);
 	return (unsigned int)id;
 
@@ -526,8 +530,9 @@ void board_init_r(gd_t *id, ulong dest_addr)
 		print_size(flash_size, "\n");
 # endif /* CONFIG_SYS_FLASH_CHECKSUM */
 	} else {
-		puts(failed);
-		hang();
+		puts("0 KB\n");
+		//puts(failed);
+		//hang();
 	}
 #endif
 
--- drivers/mtd/jedec_flash.c
+++ drivers/mtd/jedec_flash.c
@@ -367,6 +367,24 @@ static const struct amd_flash_info jedec_table[] = {
 		}
 	},
 #endif
+/* JZ2440v2使用的是MT29LV160DB */
+{
+		.mfr_id		= (u16)MX_MANUFACT,	//factory id
+		.dev_id		= 0x2249,			//device id
+		.name		= "MXIC MT29LV160DB",
+		.uaddr		= {
+			[1] = MTD_UADDR_0x0555_0x02AA /* x16, NOR flash 解锁地址 */
+		},
+		.DevSize	= SIZE_2MiB,
+		.CmdSet		= P_ID_AMD_STD,
+		.NumEraseRegions= 4,
+		.regions	= {
+			ERASEINFO(16*1024, 1),
+			ERASEINFO(8*1024,  2),
+			ERASEINFO(32*1024, 1),
+			ERASEINFO(64*1024, 31)
+		}
+	},
 };
 
 static inline void fill_info(flash_info_t *info, const struct amd_flash_info *jedec_entry, ulong base)
